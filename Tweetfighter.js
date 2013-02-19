2bd843fa4fe58fe515eed28a9c497f2a78d0c929
Osmond <Kingozzy24@gmail.com>
2013-02-18 20:33:10 -0800

    started work on getting real trends to show up
    additional work done on node.js server, still not showing resources
    because routing isn't set up yet. also site doesn't seem to run properly
    on internet explorer and firefox, we will have to look at that before we
    put it up

--- Tweetfighter.js
+++ Tweetfighter.js
@@ -1,13 +1,4 @@
-//var and requirements
-var express = require ('express');
+var express = require('express');
 var app = express();
-
-
-//serve pages
-app.get("/", function(req, page){
-	page.sendfile("twitterAPITest.html");
-});
-
-//config
-app.listen(12365);
-console.log("port 12365");
+app.use(express.static(__dirname + ''));
+app.listen(8080);
