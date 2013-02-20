692e4e65503f23e17ae72704e6d5b70416ee6656
Osmond <Kingozzy24@gmail.com>
2013-02-19 23:28:35 -0800

    multiple fixes
    Node.js server works now....
    fixed the bug where the time for firefox and IE didn't work however
    sometimes the images do not load still

--- Tweetfighter.js
+++ Tweetfighter.js
@@ -1,4 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
-app.use(express.static(__dirname + ''));
-app.listen(8080);
+app.use(express.static(__dirname + '/'));
+app.get('/', function(req, res){
+    fs.readFile(__dirname + '/twitterAPITest.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+app.listen(8888);
