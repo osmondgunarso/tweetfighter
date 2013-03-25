8599ee8d564a0743cac2cc0ce0c794e94bdcf897
Osmond <Kingozzy24@gmail.com>
2013-03-25 13:28:07 -0700

    compressed and compiled

--- scripts/twitter.js
+++ scripts/twitter.js
@@ -1,27 +1,5 @@
 //idealy nothing in this file will ever use DOM
 		//twitter
-		function Player(hash) {
-			this.hash = hash;
-			this.health = 100;
-			this.pastInformation = {tweet:0, face:0};
-			this.numTweets = 0;
-			this.numFace = 0;
-			this.sprites = [];
-			this.state = 0;
-			this.count = 0;
-		}
-		
-		Player.prototype.numTot = function(){
-			return this.numTweets + this.numFace;
-		}
-		
-		Player.prototype.createFirst = function() {
-			return this.hash + "</br><div id='stats"+this.hash+"'>" + "Health: " + this.health + ", Tweets: " + this.numTweets + "</div><div class='progress progress-striped active'> <div class='bar' id = '"+ this.hash +"' style='width: " + this.health +"%'></div> </div>";
-		}
-		
-		Player.prototype.toString = function(){
-			return "Health: " + this.health + ", Tweets: " + this.numTweets;
-		}
 		
 		function firstUpdate(player) {
 			var req = $.ajax({
