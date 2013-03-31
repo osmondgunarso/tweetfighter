fa97f2368701f397c24b27bdc155ef8f21db6618
Osmond <Kingozzy24@gmail.com>
2013-03-30 19:04:27 -0700

    node.js, socket.io, twitter backend trend calls,

--- Tweetfighter.js
+++ Tweetfighter.js
@@ -1,34 +1,46 @@
 //requirements
 var express = require('express');
-var http = require('http').createServer(app);
-var fs = require('fs');
 var app = express();
-var io = require('socket.io').listen(http);
-var port = 8888;
+var http = require('http');
+var server = http.createServer(app);
+var fs = require('fs');
+var io = require('socket.io').listen(server);
+
+
+http.createServer(app).listen(888);
+
 //create server
+app.use(express.compress());
 app.use(express.static(__dirname + '/', { maxAge: 31557600000 }));
 app.get('/', function(req, res){
-    fs.readFile(__dirname + '/twitterAPITest.html', 'utf8', function(err, text){
+    fs.readFile(__dirname + '/tweetfighterbefore.html', 'utf8', function(err, text){
 		res.send(text);
     });
 });
-app.listen(port);
-console.log(port);
 
-//socket stuff
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+	//socket stuff goes in here
+	socket.emit('news', { hello: 'world' });
+	
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
 });
 
 //twitter api calls
 
 function getTrends(){
-	var trend = 'https://api.twitter.com/1/trends/weekly.json';
+	var trend = 'http://api.twitter.com/1/trends/weekly.json';
 	http.get(trend, function(res) {
+		var data = '';
 		console.log("Got response: " + res.statusCode);
+		res.on("data", function(chunk) {
+			data += chunk
+		});
+		res.on('end', function(){
+			var obj = JSON.parse(data);
+			console.log(obj);
+		});
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
 	});
@@ -37,3 +49,9 @@ function getTrends(){
 function getTweets(){
 	
 }
+
+function calculateTweets(){
+
+}
+
+getTrends();
